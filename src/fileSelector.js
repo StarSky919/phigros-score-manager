6b6f3316bb2cb3a28df5b6235c1b5f94940c7968
ss919p@gmail.com <ss919p@gmail.com>
2022-10-20 01:11:53 +0800

    尝试修复一个Bug

--- src/fileSelector.js
+++ src/fileSelector.js
@@ -1,12 +1,16 @@
-import { createElement } from './utils.js';
+import { $, createElement } from './utils.js';
+
+const select = $('select');
 
 export function selectFile(mime, callback) {
-  const selectFile = createElement('input');
+  /*const selectFile = createElement('input');
   selectFile.type = 'file';
-  selectFile.accept = mime;
-  selectFile.addEventListener('change', event => {
+  selectFile.accept = mime;*/
+  function cb(event) {
     const file = event.target.files[0];
     callback(file);
-  });
-  selectFile.click();
+    select.removeEventListener('change', cb);
+  }
+  select.addEventListener('change', cb);
+  select.click();
 }
