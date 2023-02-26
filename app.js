b1a18c1a67e221ed907ba0f699fb12403ee8aad3
ss919p@gmail.com <ss919p@gmail.com>
2023-02-27 02:49:15 +0800

    修复了统计信息中游戏版本号的显示问题

--- app.js
+++ app.js
@@ -308,7 +308,7 @@ fetch('https://website-assets.starsky919.xyz/phigros/pgrData.json').then(res => 
     const lv16 = b19.filter(({ id, dn }) => songData[id].charts[dn].level === 16);
     const lv15 = b19.filter(({ id, dn }) => songData[id].charts[dn].level === 15);
     const result = [];
-    result.push(`截至Phigros 2.4.3版本，\n共有${songTotal}首歌曲，${chartTotal}张谱面。`);
+    result.push(`截至Phigros ${pgrVersion}版本，\n共有${songTotal}首歌曲，${chartTotal}张谱面。`);
     if (songPlayedTotal === 0 && chartPlayedTotal === 0) result.push('您尚未游玩过任何歌曲与谱面！');
     else {
       result.push(`您已游玩过其中的${songPlayedTotal}首歌曲，${chartPlayedTotal}张谱面。`);
